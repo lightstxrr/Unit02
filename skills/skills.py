d0f9054d28bd68a3edfe79228a80af6e31e7e784
unknown <eg9682@rit.edu>
2026-09-18 19:57:46 +0200

    Exercise 2

--- skills/skills.py
+++ skills/skills.py
@@ -5,3 +5,15 @@ points_gained = 10
 points_lost = 5
 final_score = points + points_gained - points_lost
 print(final_score)
+
+""" Exercise 2 """
+
+recipe_serves = 4
+num_eggs = 2
+cheese_ounces = 4
+num_guests = 6
+
+eggs_needed = (num_eggs / recipe_serves) * num_guests
+cheese_needed = (cheese_ounces / recipe_serves) * num_guests
+print("Eggs needed:", eggs_needed)
+print("Cheese needed:", cheese_needed)
